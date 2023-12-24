e8af7bf6ea52a31a5ac66c416031e69c48ffe396
south-devel <south-devel@south.com>
2023-12-25 03:31:39 +0900

    Updates

---
+++ component---src-pages-404-tsx-450cad6f1bfe731a17e8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkminimal_blog=self.webpackChunkminimal_blog||[]).push([[218],{2513:function(e,l,n){n.r(l);var t=n(7294),u=n(6463);l.default=e=>t.createElement(u.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-450cad6f1bfe731a17e8.js.map
